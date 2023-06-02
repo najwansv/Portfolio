b74f4c61e92cf9f231db75c772eebeb0b7029a06
najwansv <najwan.joan.sv@gmail.com>
2023-06-02 19:31:31 +0700

    update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Lonely
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-lonely/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
